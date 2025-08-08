061424c5a30f137d2118a1208b06454c208ae563
Levi Gagne <67981022+Levi-Gagne@users.noreply.github.com>
2025-08-08 10:07:19 -0500

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,33 +1,17 @@
-So when it created the app the tree was like this:
-
-
-profilon/databricks-hello-world-app/app.py, requirements.txt, app.yaml
-
-Here is the conentes of requirments.py and app.yaml:
-requirments.txt:
 streamlit==1.38.0
+databricks-sdk>=0.30
+PyYAML>=6.0
+# If you decide to query a Warehouse directly (optional):
+# databricks-sql-connector>=3.0
+
+#####
 
 
-app.yaml:
+
 command: [
-  "streamlit", 
+  "streamlit",
   "run",
-  "app.py"
+  "src/profilon/app.py",
+  "--server.headless", "true",
+  "--browser.gatherUsageStats", "false"
 ]
-
-
-
-
-So a few things i was thinking of reformating my app to be like this strucutre:
-
-in the root of the project i would have:
-.gitignore
-LICENSE
-NOTICE
-README.md
-(above files were already included in app and were at rood)
-requirments.txt (consiginering moving this to root and not same folder as app.py; is that a problem?)
-src/profilon/app.py, app.yaml
-src/profilon/pages/1_configure_&_run.py
-
-what are your thoughts on this refactor?
